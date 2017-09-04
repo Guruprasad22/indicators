90b8d7089088827a9a759b5773a49355f6ce5a2e
Guruprasad22 <guru.bobbi@gmail.com>
2017-09-04 13:50:38 -0400

    fixed shell script

--- src/main/resources/bhavdownload.sh
+++ src/main/resources/bhavdownload.sh
@@ -6,26 +6,27 @@
 
 stringGen()
 {
-	export dateString=$(date -d $1 +%d%b%Y | tr 'a-z' 'A-Z')
-	export yr=$(date -d $1 +%Y)
-	export mnt=$(date -d $1 +%b | tr 'a-z' 'A-Z')
+      export dateString=$(date -d $1 +%d%b%Y | tr 'a-z' 'A-Z')
+      export yr=$(date -d $1 +%Y)
+      export mnt=$(date -d $1 +%b | tr 'a-z' 'A-Z')
 }
 
 dateRangeFunction()
 {
-	startDate=$1
-	endDate=$2
-	while [[ $startDate -le $endDate ]] 
-	do 
-		echo "$startDate"
-		stringGen $startDate
-		curl --user-agent "Mozilla/4.0" "$url$yr/$mnt/cm$dateString$suffix" > $outPutFile
-		if [ $? -eq 0 ]; then
-			unzip ./$outPutFile
-			rm -f ./$outPutFile
-		fi
-		startDate=$(date -d "$startDate + 1 days" +%Y%m%d)
-	done
+      startDate=$1
+      endDate=$2
+      while [[ $startDate -le $endDate ]]
+      do
+        echo "$startDate"
+        stringGen $startDate
+        curl  -o cm$dateString$suffix "$url$yr/$mnt/cm$dateString$suffix"
+        if [ $? -eq 0 ]; then
+                unzip ./cm$dateString$suffix
+                rm -f ./cm$dateString$suffix
+        fi
+
+        startDate=$(date -d "$startDate + 1 days" +%Y%m%d)
+      done
 }
 
 export url=https://nse-india.com/content/historical/EQUITIES/
@@ -33,5 +34,6 @@ export suffix=bhav.csv.zip
 export outPutFile=myfile.zip
 dateRangeFunction $1 $2
 cp $PWD/*.csv $3
+tail -q -n +2 $3/cm*.csv > $3/output.csv
 exit 0
 
